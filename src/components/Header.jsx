fb672edbd47f2068983e89d158e790e9ad5d10eb
rockwoodfrank <rockwoodfrank@gmail.com>
2026-08-22 22:18:31 -0700

    More responsive header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,10 @@ export default function Header()
 {
     return (
         <>
-            <header className="flex justify-center bg-cyan-900 h-20">
-                <div className="w-full max-w-250 mx-10 flex items-center">
+            <header className="flex justify-center h-20 bg-linear-to-r from-cyan-900 from-50% to-amber-600 to-50%">
+                <div className="w-full max-w-250 mx-10 flex items-center bg-cyan-900">
                     <a className="font-bold text-3xl text-white" href="/">Rockwood Frank</a>
                 </div>
-                <div className="absolute top-0 right-0 w-40 h-20 bg-amber-600 border-l-25 border-white"/>
             </header>
             
         </>
